3f119250b80d8e0d0e5676892a8ec622f7a5d3ed
elifred2022 <elifed21@hotmail.com>
2025-06-22 20:24:24 -0300

    hecho el formulario y ya renderiza las tareas cargadas

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,9 +1,38 @@
+"use client";
+import { useRouter } from "next/navigation"; 
 
 
 function NewPage() {
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const title = e.target.title.value
+    const description = e.target.description.value
+
+    const res = await fetch('/api/tasks', {
+      method: 'POST',
+      body: JSON.stringify({title, description}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json()
+    console.log(data);
+
+    router.push("/");
+  }
   return (
-    <div>
-      Formulario de tareas
+    <div className="h-screen flex justify-center items-center">
+        <form className="bg-slate-800 p-10 lg:w-1/2 md:w-full" onSubmit={onSubmit}>
+          <label htmlFor="title" className="font-bold text-sm">Titulo de la tarea</label>
+          <input id="title" placeholder="tarea" type="text" className=" border border-gray-400 p-2 mb-4 w-full"/>
+          <label htmlFor="description" className="font-bold text-sm">Descripcion de la tarea</label>
+          <textarea id="description" placeholder="describe tu tarea" rows="3" className=" border border-gray-400 p-2 mb-4 w-full"></textarea>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">crear</button>
+        </form>
+      
+      
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,38 @@
+import {prisma} from '@/libs/prisma'
 
+async function loadTasks() {
+  // obtener de la base de datos
+  /*const res = await fetch('http://localhost:3000/api/tasks')
+  //const data = await res.json()
+  //console.log(data);*/
 
-function HomePage() {
+  //mejor metodo;
+  return await prisma.task.findMany()
+ // console.log(tasks);
+
+}
+
+async function HomePage() {
+  const tasks = await loadTasks()
+  console.log(tasks);
+  
   return (
-    <div>
-      Home Page
+    <section className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-3 mt-10'>
+      {tasks.map((task)=>(
+        <div key={task.id} className='bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer'>
+          <h3 className='font-bolt text-2xl mb-2'>{task.title}</h3>
+          <p>{task.description} </p>
+          <p>{new Date(task.createdAt).toLocaleDateString()} </p>
+        </div>
+      ))
+
+      }
+     
     </div>
+
+    </section>
+    
   )
 }
 
