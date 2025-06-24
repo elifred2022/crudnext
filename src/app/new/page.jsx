f99f7b8e0d2b5bedb776a5b544ba0452a09f4129
elifred2022 <elifed21@hotmail.com>
2025-06-24 20:38:41 -0300

    terminada funcion actualizar

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,16 +1,39 @@
 "use client";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-
-function NewPage() {
+function NewPage({params}) {
   const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+fetch(`/api/tasks/${params.id}`)
+    .then(res => res.json())
+    .then(data => {
+      setTitle(data.title)
+      setDescription(data.description)
+    })
+    }
+    
+  }, [])
 
   const onSubmit = async (e) => {
     e.preventDefault()
-    const title = e.target.title.value
-    const description = e.target.description.value
-
-    const res = await fetch('/api/tasks', {
+    
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`,{
+        method: "PUT",
+        body: JSON.stringify({title, description}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      const data = await res.json();
+      console.log(data);
+    } else {
+ const res = await fetch('/api/tasks', {
       method: 'POST',
       body: JSON.stringify({title, description}),
       headers: {
@@ -18,17 +41,17 @@ function NewPage() {
       }
     })
     const data = await res.json()
-    console.log(data);
-
+    }
+    router.refresh()  
     router.push("/");
   }
   return (
     <div className="h-screen flex justify-center items-center">
         <form className="bg-slate-800 p-10 lg:w-1/2 md:w-full" onSubmit={onSubmit}>
           <label htmlFor="title" className="font-bold text-sm">Titulo de la tarea</label>
-          <input id="title" placeholder="tarea" type="text" className=" border border-gray-400 p-2 mb-4 w-full"/>
+          <input onChange={(e)=> setTitle(e.target.value)} value={title} id="title" placeholder="tarea" type="text" className=" border border-gray-400 p-2 mb-4 w-full"/>
           <label htmlFor="description" className="font-bold text-sm">Descripcion de la tarea</label>
-          <textarea id="description" placeholder="describe tu tarea" rows="3" className=" border border-gray-400 p-2 mb-4 w-full"></textarea>
+          <textarea onChange={(e)=> setDescription(e.target.value)} value={description} id="description" placeholder="describe tu tarea" rows="3" className=" border border-gray-400 p-2 mb-4 w-full"></textarea>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">crear</button>
         </form>
       
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import {prisma} from '@/libs/prisma'
+import TaskCard from '@/components/TaskCard';
 
 async function loadTasks() {
   // obtener de la base de datos
@@ -20,11 +21,7 @@ async function HomePage() {
     <section className='container mx-auto'>
       <div className='grid grid-cols-3 gap-3 mt-10'>
       {tasks.map((task)=>(
-        <div key={task.id} className='bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer'>
-          <h3 className='font-bolt text-2xl mb-2'>{task.title}</h3>
-          <p>{task.description} </p>
-          <p>{new Date(task.createdAt).toLocaleDateString()} </p>
-        </div>
+        <TaskCard task={task} key={task.id} />
       ))
 
       }
